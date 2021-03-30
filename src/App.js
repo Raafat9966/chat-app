04cc2543e348533e01f2655ed0a9429a80a15c90
Raafat Basheer <raafat.basheer@yahoo.com>
2021-03-30 21:27:17 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ChatEngine
+			height="100vh"
+			userName="faust"
+			userSecret="123456"
+			projectID="e95c57f0-a63c-43f4-aa46-8d9f75ab14de"
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		/>
+	);
 }
-
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ChatFeed(props) {
+	console.log(props);
+	return (
+		<div>
+			<h1>chat feed</h1>
+		</div>
+	);
+}
+
+export default ChatFeed;
